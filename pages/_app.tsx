064fbfdeb3cd4e9d815deb3b293b9ed876ac3136
Weil <f0955710119@gmail.com>
2022-04-10 20:28:26 +0800

    Init project with global style set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createGlobalStyle } from "styled-components";
+import type { AppProps } from "next/app";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: openhuninn;
+    src: url('/fonts/jf-openhuninn-1.1.ttf') format('truetype');
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: openhuninn;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
