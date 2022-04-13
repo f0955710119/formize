4b22a881c3b9769d0f8c9cc7de7e3e14eac8f6b1
Weil <f0955710119@gmail.com>
2022-04-13 22:43:41 +0800

    Setup reducer and actions for form style with redux toolkit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { settingReducer } from "./slice/settingSlice";
 import { questionReducer } from "./slice/questionSlice";
+import { styleReducer } from "./slice/styleSlice";
 
 export const store = configureStore({
   reducer: {
     setting: settingReducer,
     question: questionReducer,
+    style: styleReducer,
   },
 });
 
---
+++ src/store/slice/styleSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import reducers from "../reducer/styleReducer";
+
+export interface StyleState {
+  theme: number;
+  fontTraditional: number;
+  fontEnglish: number;
+  backgroundImages: string[];
+}
+
+const initialState: StyleState = {
+  theme: 0,
+  fontTraditional: 0,
+  fontEnglish: 0,
+  backgroundImages: [""],
+};
+
+const styleSlice = createSlice({
+  name: "style",
+  initialState,
+  //  感覺這包reducer能直接改寫
+  reducers,
+});
+
+export const styleReducer = styleSlice.reducer;
+export const styleActions = styleSlice.actions;
