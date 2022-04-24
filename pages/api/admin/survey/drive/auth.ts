1d2aefb6de194e58055862b85dad0a5bf2106e41
Weil <f0955710119@gmail.com>
2022-04-25 03:46:02 +0800

    Close drive api route and uninstall useless package

--- pages/api/admin/survey/drive/auth.ts
+++ pages/api/admin/survey/drive/auth.ts
@@ -1,31 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-const fs = require("fs");
-const { google } = require("googleapis");
-import credentials from "../../../../../credentials.json";
+
+// const { google } = require("googleapis");
+// import credentials from "../../../../../credentials.json";
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const clientId = credentials.web.client_id;
-const clientSecret = credentials.web.client_secret;
-const redirectUri =
-  process.env.NEXT_PUBLIC_ENV === "development"
-    ? credentials.web.redirect_uris[1]
-    : credentials.web.redirect_uris[0];
+// const clientId = credentials.web.client_id;
+// const clientSecret = credentials.web.client_secret;
+// const redirectUri =
+//   process.env.NEXT_PUBLIC_ENV === "development"
+//     ? credentials.web.redirect_uris[1]
+//     : credentials.web.redirect_uris[0];
 
-const oAuth2Client = new google.auth.OAuth2({
-  clientId,
-  clientSecret,
-  redirectUri,
-});
+// const oAuth2Client = new google.auth.OAuth2({
+//   clientId,
+//   clientSecret,
+//   redirectUri,
+// });
 
-const TOKEN_PATH = "exists-token.json";
 
-// For google config
-const SCOPES = [
-  "https://www.googleapis.com/auth/drive.appdata",
-  "https://www.googleapis.com/auth/drive.file",
-];
+// // For google config
+// const SCOPES = [
+//   "https://www.googleapis.com/auth/drive.appdata",
+//   "https://www.googleapis.com/auth/drive.file",
+// ];
 
 interface Data {
   status: string;
@@ -42,51 +41,54 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   if (req.method === "GET") {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: SCOPES,
-    });
-
     res.status(200).json({
       status: "success",
       status_code: 200,
       message: "get drive access successfully!",
-      data: {
-        uri: authUrl,
-      },
     });
+    // const authUrl = oAuth2Client.generateAuthUrl({
+    //   access_type: "offline",
+    //   scope: SCOPES,
+    // });
+
+    // res.status(200).json({
+    //   status: "success",
+    //   status_code: 200,
+    //   message: "get drive access successfully!",
+    //   data: {
+    //     uri: authUrl,
+    //   },
+    // });
   }
 
-  if (req.method === "POST") {
-    if (req.body.code === null) {
-      res.status(400).json({
-        status: "fail",
-        status_code: 400,
-        message: "there is no code to access token",
-      });
-      return;
-    }
+  // if (req.method === "POST") {
+  //   if (req.body.code === null) {
+  //     res.status(400).json({
+  //       status: "fail",
+  //       status_code: 400,
+  //       message: "there is no code to access token",
+  //     });
+  //     return;
+  //   }
 
-    try {
-      const { tokens } = await oAuth2Client.getToken(req.body.code);
-
-      // fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
-      oAuth2Client.setCredentials(tokens);
-      res.status(200).json({
-        status: "success",
-        status_code: 200,
-        message: "get drive access successfully!",
-        data: {
-          ...tokens,
-        },
-      });
-    } catch (error: any) {
-      console.error(error.message);
-      res.status(400).json({
-        status: "fail",
-        status_code: 400,
-        message: "fail to get token: " + error.message,
-      });
-    }
-  }
+  //   try {
+  //     const { tokens } = await oAuth2Client.getToken(req.body.code);
+  //     oAuth2Client.setCredentials(tokens);
+  //     res.status(200).json({
+  //       status: "success",
+  //       status_code: 200,
+  //       message: "get drive access successfully!",
+  //       data: {
+  //         ...tokens,
+  //       },
+  //     });
+  //   } catch (error: any) {
+  //     console.error(error.message);
+  //     res.status(400).json({
+  //       status: "fail",
+  //       status_code: 400,
+  //       message: "fail to get token: " + error.message,
+  //     });
+  //   }
+  // }
 }
--- pages/api/admin/survey/drive/image.ts
+++ pages/api/admin/survey/drive/image.ts
@@ -1,157 +1,99 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { google } from "googleapis";
-import nextConnect from "next-connect";
-import multer from "multer";
-import credentials from "../../../../../credentials.json";
-import path from "path";
-import fs from "fs";
-import formidable from "formidable";
+// import { google } from "googleapis";
+// import nextConnect from "next-connect";
+// import credentials from "../../../../../credentials.json";
 
-const form = formidable({ multiples: false });
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-async function parseMultipartForm(req: any, res: any, next: any) {
-  const contentType = req.headers["content-type"];
-  if (contentType && contentType.indexOf("multipart/form-data") !== -1) {
-    form.parse(req, (err, fields, file) => {
-      if (!err) {
-        req.body = fields; // sets the body field in the request object
-        req.file = file; // sets the files field in the request object
-      }
-      next(); // continues to the next middleware or to the route
-    });
-  } else {
-    next();
-  }
-}
+// // Google setup
+// const clientId = credentials.web.client_id;
+// const clientSecret = credentials.web.client_secret;
+// const redirectUri =
+//   process.env.NEXT_PUBLIC_ENV === "development"
+//     ? credentials.web.redirect_uris[1]
+//     : credentials.web.redirect_uris[0];
 
-const middleware = nextConnect();
+// const oAuth2Client = new google.auth.OAuth2({
+//   clientId,
+//   clientSecret,
+//   redirectUri,
+// });
 
-middleware.use(parseMultipartForm);
+// // For google config
+// const SCOPES = [
+//   "https://www.googleapis.com/auth/drive.appdata",
+//   "https://www.googleapis.com/auth/drive.file",
+// ];
 
-const dotenv = require("dotenv");
-dotenv.config();
+// // Multer
+// const handler = nextConnect();
 
-// Google setup
-const clientId = credentials.web.client_id;
-const clientSecret = credentials.web.client_secret;
-const redirectUri =
-  process.env.NEXT_PUBLIC_ENV === "development"
-    ? credentials.web.redirect_uris[1]
-    : credentials.web.redirect_uris[0];
+// interface Data {
+//   status: string;
+//   status_code: number;
+//   message: string;
+//   data?: {
+//     imageURL?: string;
+//   };
+// }
 
-const oAuth2Client = new google.auth.OAuth2({
-  clientId,
-  clientSecret,
-  redirectUri,
-});
+// handler.post(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+//   const token = JSON.parse(JSON.stringify(req.body.token));
+//   const file = (req as any).file;
+//   console.log(token.access_token);
+//   console.log(file);
 
-// For google config
-const SCOPES = [
-  "https://www.googleapis.com/auth/drive.appdata",
-  "https://www.googleapis.com/auth/drive.file",
-];
+//   if (token === null || token.access_token === "") {
+//     res.status(400).json({
+//       status: "fail",
+//       status_code: 400,
+//       message: "can't upload image without token",
+//     });
+//     return;
+//   }
 
-// Multer
-const handler = nextConnect();
+//   oAuth2Client.setCredentials(token);
+//   const form = new FormData();
+//   const fileMetaData = {
+//     name: file.file.originalFilename,
+//     mimeType: file.file.mimetype,
+//   };
+//   form.append(
+//     "metadata",
+//     new Blob([JSON.stringify(fileMetaData)], { type: "application/json" })
+//   );
+//   form.append("file", file);
 
-// const storage = multer.diskStorage({
-//   filename(req, file, callback) {
-//     callback(
-//       null,
-//       file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-//     );
+//   // await uploadImage(token.access_token, form);
+//   // const drive = google.drive({ version: "v3", auth: oAuth2Client });
+
+//   // const fileMetaData = {
+//   //   name: file.name,
+//   // };
+
+//   // const media = {
+//   //   mimeType: file.type,
+//   //   body: file,
+//   // };
+
+//   // drive.files.create({
+//   //   requestBody: fileMetaData,
+//   //   media,
+//   //   fields: "id",
+//   // });
+
+//   res.status(201).json({
+//     status: "success",
+//     status_code: 201,
+//     message: "upload file successfully",
+//   });
+// });
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
 //   },
-// });
+// };
 
-// const upload = multer({
-//   storage,
-// });
-
-async function uploadImage(accessToken: string, form: any) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(
-    "post",
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id"
-  );
-  xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-  xhr.responseType = "json";
-  xhr.onload = () => {
-    console.log(xhr.response.id);
-  };
-  xhr.send(form);
-}
-interface Data {
-  status: string;
-  status_code: number;
-  message: string;
-  data?: {
-    imageURL?: string;
-  };
-}
-// const upload = multer({ dest: "uploads/" });
-
-// const uploadFile = upload.single("file");
-
-handler.use(middleware);
-handler.post(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // console.log(JSON.parse(JSON.stringify(req.body.token)));
-  // console.log((req as any).file);
-
-  const token = JSON.parse(JSON.stringify(req.body.token));
-  const file = (req as any).file;
-  console.log(token.access_token);
-  console.log(file);
-
-  if (token === null || token.access_token === "") {
-    res.status(400).json({
-      status: "fail",
-      status_code: 400,
-      message: "can't upload image without token",
-    });
-    return;
-  }
-
-  oAuth2Client.setCredentials(token);
-  const form = new FormData();
-  const fileMetaData = {
-    name: file.file.originalFilename,
-    mimeType: file.file.mimetype,
-  };
-  form.append(
-    "metadata",
-    new Blob([JSON.stringify(fileMetaData)], { type: "application/json" })
-  );
-  form.append("file", file);
-
-  // await uploadImage(token.access_token, form);
-  // const drive = google.drive({ version: "v3", auth: oAuth2Client });
-
-  // const fileMetaData = {
-  //   name: file.name,
-  // };
-
-  // const media = {
-  //   mimeType: file.type,
-  //   body: file,
-  // };
-
-  // drive.files.create({
-  //   requestBody: fileMetaData,
-  //   media,
-  //   fields: "id",
-  // });
-
-  res.status(201).json({
-    status: "success",
-    status_code: 201,
-    message: "upload file successfully",
-  });
-});
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default handler;
+// export default handler;
