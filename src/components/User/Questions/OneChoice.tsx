cdf8fc02ba213a7a121e656b90bdac077ef2afc8
Weil <f0955710119@gmail.com>
2022-05-07 17:27:31 +0800

    Add style to one choice question of user form

--- src/components/User/Questions/OneChoice.tsx
+++ src/components/User/Questions/OneChoice.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import styled from "styled-components";
 import {
   FormControl,
   RadioGroup,
@@ -10,6 +11,83 @@ import { userActions } from "../../../store/slice/userSlice";
 import useGetQuestionIdIndex from "../../../hooks/useGetQuestionIdIndex";
 import { useAppSelector } from "../../../hooks/useAppSelector";
 
+const CustomFormControl = styled(FormControl)`
+  width: 100%;
+  display: block;
+  & .css-z7bf87-MuiFormGroup-root {
+    /* flex-direction: row; */
+  }
+`;
+
+const CustomRadioGroup = styled(RadioGroup)`
+  align-items: end;
+  /* display: flex;
+  align-items: center;
+  width: 100%;
+  -webkit-flex-direction: row; */
+`;
+
+interface CustomFormControlLabelProps {
+  isActive: boolean;
+}
+// prettier-ignore
+const CustomFormControlLabel = styled(FormControlLabel)<CustomFormControlLabelProps>`
+  margin-bottom: 1rem;
+  padding: 0.8rem 0;
+  width: 97.96%;
+  font-family: inherit;
+  border: 2px solid ${(props) => 
+    props.isActive ? props.theme.title : `${props.theme.title}60`
+  };
+  border-radius: 5px;
+  transition: border 0.3s;
+  & .css-ahj2mt-MuiTypography-root {
+    font-size: 1.8rem;
+    font-family: inherit;
+    margin-left: 1rem;
+    transform: translateY(0.1rem);
+  }
+`;
+
+const CustomRadio = styled(Radio)`
+  /* font-size: 2rem; */
+`;
+
+const CustomRadioIcon = styled.span`
+  width: 2rem;
+  height: 2rem;
+  background-color: ${(props) => props.theme.title};
+  opacity: 0.5;
+  border-radius: 50%;
+  transition: opacity 0.3s;
+
+  input:hover ~ & {
+    background-color: "#30404d";
+    opacity: 1;
+    background-image: radial-gradient(#fff, #fff 28%, transparent 32%);
+  }
+
+  input:checked ~ & {
+    opacity: 1;
+  }
+`;
+const CustonCheckedIcon = styled(CustomRadioIcon)`
+  background-color: ${(props) => props.theme.title};
+
+  &::before {
+    content: "";
+    display: block;
+    width: 2rem;
+    height: 2rem;
+
+    background-image: radial-gradient(#fff, #fff 28%, transparent 32%);
+  }
+
+  input:hover ~ & {
+    background-color: "#30404d";
+  }
+`;
+
 interface OneChoiceProps {
   options: string[];
   questionId: string;
@@ -20,8 +98,8 @@ const OneChoice: FC<OneChoiceProps> = ({ options, questionId }) => {
   const { answers } = useAppSelector((state) => state.user);
   const questionIdIndex = useGetQuestionIdIndex(questionId);
   return (
-    <FormControl>
-      <RadioGroup
+    <CustomFormControl>
+      <CustomRadioGroup
         aria-labelledby="one-choice-question"
         name="one-choice-question-radio-buttons-group"
         onChange={(event) => {
@@ -30,21 +108,25 @@ const OneChoice: FC<OneChoiceProps> = ({ options, questionId }) => {
         }}
       >
         {options.map((option, i) => (
-          <FormControlLabel
+          <CustomFormControlLabel
+            isActive={answers[questionIdIndex].input === `${i + 1}.${option}`}
             value={`${i + 1}.${option}`}
             control={
-              <Radio
+              <CustomRadio
                 checked={
                   answers[questionIdIndex].input === `${i + 1}.${option}`
                 }
+                icon={<CustomRadioIcon />}
+                checkedIcon={<CustonCheckedIcon />}
+                size="medium"
               />
             }
             label={option}
             key={i}
           />
         ))}
-      </RadioGroup>
-    </FormControl>
+      </CustomRadioGroup>
+    </CustomFormControl>
   );
 };
 
