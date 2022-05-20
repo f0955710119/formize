0e41d7947bd0cc2d492a9b1e6ed14d26fd1220ae
Weil <f0955710119@gmail.com>
2022-05-20 22:41:48 +0800

    Turn select that is used to switch form mode into a component

--- src/components/AdminPage/CreateForm/Setting/SectionNormal.tsx
+++ src/components/AdminPage/CreateForm/Setting/SectionNormal.tsx
@@ -1,65 +1,17 @@
-import { FC, ChangeEvent, useState, useEffect, useRef } from "react";
+import { FC, ChangeEvent, useContext } from "react";
 
-import { FormControl, Select, MenuItem } from "@mui/material";
-import styled from "styled-components";
-
-import settingConfig from "../../../../configs/settingConfig";
-import useAppDispatch from "../../../../hooks/useAppDispatch";
-import useAppSelector from "../../../../hooks/useAppSelector";
 import settingActionType from "../../../../store/actionType/settingActionType";
-import { questionActions } from "../../../../store/slice/questionSlice";
-import { settingActions } from "../../../../store/slice/settingSlice";
-import sweetAlert from "../../../../utils/sweetAlert";
 import Field from "../UI/Field";
 import Label from "../UI/Label";
 import SectionWrapper from "../UI/Section";
 import SectionHeading from "../UI/SectionHeading";
 import NormalTextInput from "./SectionNormal/NormalTextInput";
-
-const { DEFAULT_MODE_LIST } = settingConfig;
-
-const CustomedFormControl = styled(FormControl)`
-  width: calc(100% - 12rem);
-  height: 100%;
-  border-radius: 0px;
-
-  & div {
-    border-radius: 0px;
-    height: 100%;
-  }
-`;
+import { settingContext } from "../../../../store/context/settingContext";
+import ModeSelect from "./SectionNormal/ModeSelect";
 
 const SectionNormal: FC = () => {
-  const dispatch = useAppDispatch();
-  const { setting } = useAppSelector((state) => state);
-  const [selectedMode, setSelectedMode] = useState<string>(setting.mode);
-  const hasUpdateMode = useRef<boolean>(false);
-
-  const dispatchNormalSettingHandler = (
-    actionType: string,
-    value: string | number | object | null
-  ) => {
-    dispatch(settingActions.updateSingleSettingInput({ actionType, value }));
-  };
-
-  const switchModeHandler = () => {
-    dispatchNormalSettingHandler(settingActionType.MODE, selectedMode);
-    dispatch(
-      questionActions.updateQuestionPage({ page: 1, isSwitchMode: true })
-    );
-    dispatch(
-      settingActions.updateSingleSettingInput({
-        actionType: settingActionType.PAGE_QUANTITY,
-        value: 1,
-      })
-    );
-  };
-
-  useEffect(() => {
-    if (!hasUpdateMode.current) return;
-    hasUpdateMode.current = false;
-    switchModeHandler();
-  }, [selectedMode]);
+  const settingContextData = useContext(settingContext);
+  const { title, setField } = settingContextData;
 
   return (
     <>
@@ -68,50 +20,16 @@ const SectionNormal: FC = () => {
         <Field>
           <Label>標題</Label>
           <NormalTextInput
-            value={setting.title}
-            type="text"
+            value={title}
             placeholder="輸入問卷的標題"
-            changeHandler={(event: ChangeEvent<HTMLInputElement>) => {
-              const { value } = event.currentTarget;
-              dispatchNormalSettingHandler(settingActionType.TITLE, value);
-            }}
+            changeHandler={(event: ChangeEvent<HTMLInputElement>) =>
+              setField(settingActionType.TITLE, event.currentTarget.value)
+            }
           />
         </Field>
         <Field>
           <Label>頁面模式</Label>
-          <CustomedFormControl>
-            <Select value={setting.mode}>
-              {DEFAULT_MODE_LIST.map((option, i) => (
-                <MenuItem
-                  value={"" + i}
-                  key={option}
-                  onClick={() => {
-                    if (+selectedMode === i) return;
-                    const modeReminderText =
-                      +selectedMode === 0
-                        ? "改變分頁模式會完全改動當前的分頁設定，\n如:「當前的題目將全部移動至第一頁」，\n確定要改變嗎?"
-                        : "改變分頁模式會完全改動當前的分頁設定，\n如:「分頁將歸為第一頁，而當前的分頁將不被儲存」，\n確定要改變嗎?";
-
-                    sweetAlert.clickToConfirmAlert(
-                      {
-                        title: "切換模式",
-                        text: modeReminderText,
-                        cancelButtonText: "取消變動",
-                        confirmButtonText: "確定切換",
-                        imageUrl: `${process.env.NEXT_PUBLIC_ORIGIN}/images/loading-reminder.svg`,
-                      },
-                      () => {
-                        setSelectedMode("" + i);
-                        hasUpdateMode.current = true;
-                      }
-                    );
-                  }}
-                >
-                  {option}
-                </MenuItem>
-              ))}
-            </Select>
-          </CustomedFormControl>
+          <ModeSelect />
         </Field>
       </SectionWrapper>
     </>
---
+++ src/components/AdminPage/CreateForm/Setting/SectionNormal/ModeSelect.tsx
@@ -0,0 +1,112 @@
+import { FC, useContext, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import settingConfig from "../../../../../configs/settingConfig";
+import { settingContext } from "../../../../../store/context/settingContext";
+import useAppDispatch from "../../../../../hooks/useAppDispatch";
+import { questionActions } from "../../../../../store/slice/questionSlice";
+import settingActinoType from "../../../../../store/actionType/settingActionType";
+import sweetAlert from "../../../../../utils/sweetAlert";
+
+const CustomedFormControl = styled(FormControl)`
+  width: calc(100% - 12rem);
+  height: 100%;
+  border-radius: 0px;
+
+  & div {
+    border-radius: 0px;
+    height: 100%;
+  }
+`;
+
+const { DEFAULT_MODE_LIST } = settingConfig;
+
+const singlePageReminderText =
+  "改變分頁模式會完全改動當前的分頁設定，\n如:「當前的題目將全部移動至第一頁」，\n確定要改變嗎?";
+const multiplePageReminderText =
+  "改變分頁模式會完全改動當前的分頁設定，\n如:「分頁將歸為第一頁，而當前的分頁將不被儲存」，\n確定要改變嗎?";
+const reminderImagePath = `${process.env.NEXT_PUBLIC_ORIGIN}/images/loading-reminder.svg`;
+
+const ModeSelect: FC = () => {
+  const dispatch = useAppDispatch();
+  const { mode, setField } = useContext(settingContext);
+  const [selectedMode, setSelectedMode] = useState<string>(mode);
+  const hasUpdateMode = useRef<boolean>(false);
+
+  const switchModeHandler = () => {
+    setField(settingActinoType.MODE, selectedMode);
+    setField(settingActinoType.PAGE_QUANTITY, 1);
+    dispatch(
+      questionActions.updateQuestionPage({ page: 1, isSwitchMode: true })
+    );
+  };
+
+  const clickToSwitchModeHandler = (
+    modeInfo: {
+      selectedMode: string;
+      currentMode: number;
+      switchModeReminder: string;
+      reminderImagePath: string;
+    },
+    callback: () => void
+  ) => {
+    const { selectedMode, currentMode, switchModeReminder, reminderImagePath } =
+      modeInfo;
+    if (+selectedMode === currentMode) return;
+    sweetAlert.clickToConfirmAlert(
+      {
+        title: "切換模式",
+        text: switchModeReminder,
+        cancelButtonText: "取消變動",
+        confirmButtonText: "確定切換",
+        imageUrl: reminderImagePath,
+      },
+      callback
+    );
+  };
+
+  const confirmToSwitchModeCallback = (currentMode: number) => {
+    setSelectedMode("" + currentMode);
+    hasUpdateMode.current = true;
+  };
+
+  useEffect(() => {
+    if (!hasUpdateMode.current) return;
+    hasUpdateMode.current = false;
+    switchModeHandler();
+  }, [selectedMode]);
+
+  const switchModeReminder =
+    selectedMode === "0" ? singlePageReminderText : multiplePageReminderText;
+
+  return (
+    <CustomedFormControl>
+      <Select value={mode}>
+        {DEFAULT_MODE_LIST.map((option, i) => (
+          <MenuItem
+            value={`${i}`}
+            key={option}
+            onClick={() => {
+              clickToSwitchModeHandler(
+                {
+                  selectedMode,
+                  currentMode: i,
+                  switchModeReminder,
+                  reminderImagePath,
+                },
+                () => confirmToSwitchModeCallback(i)
+              );
+            }}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </CustomedFormControl>
+  );
+};
+
+export default ModeSelect;
