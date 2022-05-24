e40edd88885bb488f59a6dd8621ed570cb7bb9f4
Weil <f0955710119@gmail.com>
2022-05-25 00:53:38 +0800

    Fix bugs of displaying the last editing status of form styling to be inited

--- src/hooks/useInitNewForm.ts
+++ src/hooks/useInitNewForm.ts
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import backgroundConfig from "../configs/backgroundConfig";
+import styleActionType from "../store/actionType/styleActionType";
+import { styleContext } from "../store/context/styleContext";
 import { questionActions } from "../store/slice/questionSlice";
 import useAppDispatch from "./useAppDispatch";
 
 const useInitNewForm = () => {
   const dispatch = useAppDispatch();
+  const { setField } = useContext(styleContext);
 
   const initHandler = () => {
     dispatch(questionActions.initQuestion());
+    setField(styleActionType.THEME, "0");
+    setField(styleActionType.FONT, "0");
+    setField(styleActionType.BACKGROUND_IMAGE, backgroundConfig.YELLOW1_URL);
   };
 
   return initHandler;
